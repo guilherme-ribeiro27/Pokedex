3c4c244486fb6a501222236b930c928b553bc828
guilherme-ribeiro27 <gui062702@gmail.com>
2022-08-08 21:55:04 -0300

    Routes configured and added

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes,Route, Link } from "react-router-dom";
+import { Home, Pokedex, PokeList, Pokemon } from "./pages";
+
+export const Router = () =>{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/pokedex" element={<Pokedex />} />
+                <Route path="/pokelist" element={<PokeList />} />
+                <Route path='/pokemon' element={<Pokemon />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
