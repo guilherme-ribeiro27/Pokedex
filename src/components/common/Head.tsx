18108d44d1da2002ae1aa434e8ce7a3d6190f9bb
guilherme-ribeiro27 <gui062702@gmail.com>
2022-08-08 21:54:19 -0300

    Components folder created

---
+++ src/components/common/Head.tsx
@@ -0,0 +1,13 @@
+interface props{
+    title:string;
+}
+
+const Head = ({title}:props)=>{
+    return(
+        <head>
+            <title>{title}</title>
+            <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        </head>
+    )
+}
+export default Head;
