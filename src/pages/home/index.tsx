f56596b98178bbfded8950d86ca99bb58652a95c
guilherme-ribeiro27 <gui062702@gmail.com>
2022-08-11 21:29:00 -0300

    Home Page ended and styled

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.scss'
 import Logo from '../../assets/logo.svg';
 import Pokeball from '../../assets/pokeball.svg';
 import Rayquaza from '../../assets/rayquaza.svg';
+import { Instagram, Linkedin, GitHub } from "react-feather";
 
 const Home = () => {
     return (
@@ -11,23 +12,33 @@ const Home = () => {
             <div className={styles.page}>
                 <div className={styles.content}>
                     <img className={styles.logo} src={Logo} alt="Pokémon"/>
-                    <div>
+                    <div className={styles.textContent}>
                         <p className={styles.title}>
                             Praticando React JS <br></br>com Pokemons
                         </p>
                         <p className={styles.subTitle}>
                             Projeto de pokedex por regiões utilizando a API pública da PokeAPI
                         </p>
-                        <button>
+                        <a className={styles.btn} href="/pokelist">
                             Go to Dex
-                            <img src={Pokeball} className={styles.imgButton} alt="Pokeball" />
-                        </button>
+                            <img src={Pokeball} className={styles.imga} alt="Pokeball" />
+                        </a>
                         <div className={styles.socialMedias}>
-                            
+                            <a href="" about="_blank" >
+                                <Instagram className={styles.icon} size={35}/>
+                            </a>
+                            <a href="" about="_blank" >
+                                <Linkedin className={styles.icon} size={35}/>
+                            </a>
+                            <a href="" about="_blank" >
+                                <GitHub className={styles.icon} size={35}/>
+                            </a>
                         </div>
                     </div>
                 </div>
-                <img src={Rayquaza} className={styles.img} alt="LogoRayquaza" />
+                <div className={styles.contentImg}>
+                    <img src={Rayquaza} alt="LogoRayquaza" />
+                </div>
             </div>
         </>
     )
